36670838436cf92d0def11d1de81abbf68ffda43
AjinkyaBagmar <73771478+AjinkyaBagmar@users.noreply.github.com>
2021-03-04 21:44:00 +0530

    Update arduino_obstacle_avoidance_car_code.ino

--- arduino_obstacle_avoidance_car_code.ino
+++ arduino_obstacle_avoidance_car_code.ino
@@ -1,174 +1,134 @@
+#define mr1 6
+#define mr2  5
+#define ml1 9
+#define ml2 10
+#define servo_pulse 7
+#define triger 8
 
-#include <Servo.h>          //standard library for the servo
-#include <NewPing.h>        //for the Ultrasonic sensor function library.
+#include <Servo.h>
+int distance, select, pos=0;
+Servo myservo;
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(7);
+for(int i=0 ; i<11 ; i++)
+{
+  pinMode(i, OUTPUT);
+}
 
-//L298N motor control pins
-const int LeftMotorForward = 6;
-const int LeftMotorBackward = 7;
-const int RightMotorForward = 5;
-const int RightMotorBackward = 4;
 
-//sensor pins
-#define trig_pin A1 //analog input 1
-#define echo_pin A2 //analog input 2
+}
+int getDistance()
+// returns distance from Ping))) sensor in cm
+{
+int distance;
+unsigned long pulseduration=0;
+// get the raw measurement data from Ping)))
+// set pin as output so we can send a pulse
+pinMode(triger, OUTPUT);
+// set output to LOW
+digitalWrite(triger, LOW);
+delayMicroseconds(5);
+// send the 5uS pulse out to activate Ping)))
+digitalWrite(triger, HIGH);
+delayMicroseconds(5);
+digitalWrite(triger, LOW);
+// change the digital pin to input to read the incoming pulse
+pinMode(triger, INPUT);
+// measure the length of the incoming pulse
+pulseduration=pulseIn(triger, HIGH);
+// divide the pulse length in half
+pulseduration=pulseduration/2;
+// convert to centimeters
+distance = int(pulseduration/29);
+return distance;
+}
+  void goForward(){
 
-#define maximum_distance 200
-boolean goesForward = false;
-int distance = 100;
+  digitalWrite(mr1, HIGH);
+  digitalWrite(mr2, LOW);
+  digitalWrite(ml1, HIGH);
+  digitalWrite(ml2, LOW);
+}
+  void goBack(){
 
-NewPing sonar(trig_pin, echo_pin, maximum_distance); //sensor function
-Servo servo_motor; 
-
-void setup(){
-
-  pinMode(RightMotorForward, OUTPUT);
-  pinMode(LeftMotorForward, OUTPUT);
-  pinMode(LeftMotorBackward, OUTPUT);
-  pinMode(RightMotorBackward, OUTPUT);
+  digitalWrite(mr1,LOW);
+  digitalWrite(mr2, HIGH);
+  digitalWrite(ml1, LOW);
+  digitalWrite(ml2, HIGH);
+}
+void turnLeft()
+{ 
+  digitalWrite(mr1, HIGH);
+  digitalWrite(mr2, LOW);
+  digitalWrite(ml1, LOW);
+  digitalWrite(ml2, HIGH);
+}
+void turnRight()
+{
+  digitalWrite(mr1,LOW);
+  digitalWrite(mr2,HIGH);
+  digitalWrite(ml1,HIGH);
+  digitalWrite(ml2, LOW);
   
-  servo_motor.attach(9); //our servo pin
-
-  servo_motor.write(115);
-  delay(2000);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
 }
+void STOP()
+{
 
-void loop(){
-
-  int distanceRight = 0;
-  int distanceLeft = 0;
-  delay(50);
-
-  if (distance <= 20){
-    moveStop();
-    delay(300);
-    moveBackward();
-    delay(400);
-    moveStop();
-    delay(300);
-    distanceRight = lookRight();
-    delay(300);
-    distanceLeft = lookLeft();
-    delay(300);
-
-    if (distance >= distanceLeft){
-      turnRight();
-      moveStop();
-    }
-    else{
-      turnLeft();
-      moveStop();
-    }
-  }
-  else{
-    moveForward(); 
-  }
-    distance = readPing();
+  digitalWrite(mr1,LOW);
+  digitalWrite(mr2,LOW);
+  digitalWrite(ml1, LOW);
+  digitalWrite(ml2, LOW);
 }
+void loop() 
+{
 
-int lookRight(){  
-  servo_motor.write(50);
-  delay(500);
-  int distance = readPing();
-  delay(100);
-  servo_motor.write(115);
-  return distance;
-}
-
-int lookLeft(){
-  servo_motor.write(170);
-  delay(500);
-  int distance = readPing();
-  delay(100);
-  servo_motor.write(115);
-  return distance;
-  delay(100);
-}
-
-int readPing(){
-  delay(70);
-  int cm = sonar.ping_cm();
-  if (cm==0){
-    cm=250;
-  }
-  return cm;
-}
-
-void moveStop(){
-  
-  digitalWrite(RightMotorForward, LOW);
-  digitalWrite(LeftMotorForward, LOW);
-  digitalWrite(RightMotorBackward, LOW);
-  digitalWrite(LeftMotorBackward, LOW);
-}
-
-void moveForward(){
-
-  if(!goesForward){
-
-    goesForward=true;
+  Serial.print(distance);
+  myservo.write(90);
+  delay(15);
+  distance = int (getDistance());
+Serial.print(distance);
+  while(distance >20 ) {
+    goForward();
+    distance=getDistance();
     
-    digitalWrite(LeftMotorForward, HIGH);
-    digitalWrite(RightMotorForward, HIGH);
-  
-    digitalWrite(LeftMotorBackward, LOW);
-    digitalWrite(RightMotorBackward, LOW); 
   }
+while(distance <= 20)
+{
+  STOP();
+  delay(1000);
+  myservo.write(pos);
+  select = pos;
+  pos+=45;
+  if(pos == 225)
+  pos = 0;
+  distance=getDistance();
+  
+  }
+myservo.write(90);
+switch(select){
+    case 0:
+    turnLeft();
+    delay(250);
+    STOP();
+    break;
+    case 45 :
+    turnLeft();
+    delay(500);
+    STOP();
+    break;
+    case 135 :
+    turnRight();
+    delay(250);
+    STOP();
+    break;
+    case 180 :
+    turnRight();
+    delay(500);
+    STOP();
+    break;
+}
+  
 }
 
-void moveBackward(){
-
-  goesForward=false;
-
-  digitalWrite(LeftMotorBackward, HIGH);
-  digitalWrite(RightMotorBackward, HIGH);
-  
-  digitalWrite(LeftMotorForward, LOW);
-  digitalWrite(RightMotorForward, LOW);
-  
-}
-
-void turnRight(){
-
-  digitalWrite(LeftMotorForward, HIGH);
-  digitalWrite(RightMotorBackward, HIGH);
-  
-  digitalWrite(LeftMotorBackward, LOW);
-  digitalWrite(RightMotorForward, LOW);
-  
-  delay(500);
-  
-  digitalWrite(LeftMotorForward, HIGH);
-  digitalWrite(RightMotorForward, HIGH);
-  
-  digitalWrite(LeftMotorBackward, LOW);
-  digitalWrite(RightMotorBackward, LOW);
- 
-  
-  
-}
-
-void turnLeft(){
-
-  digitalWrite(LeftMotorBackward, HIGH);
-  digitalWrite(RightMotorForward, HIGH);
-  
-  digitalWrite(LeftMotorForward, LOW);
-  digitalWrite(RightMotorBackward, LOW);
-
-  delay(500);
-  
-  digitalWrite(LeftMotorForward, HIGH);
-  digitalWrite(RightMotorForward, HIGH);
-  
-  digitalWrite(LeftMotorBackward, LOW);
-  digitalWrite(RightMotorBackward, LOW);
-}
 
